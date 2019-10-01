84f971339e4bc88b9319d55797543707f9a1ef80
Nikita Horb <masternik007@gmail.com>
2019-10-02 01:21:36 +0300

    FCommit_1 d d lean

--- main.cpp
+++ main.cpp
@@ -12,8 +12,11 @@ int main()
         cin >> v[i];
     }
     sort(v.begin,v.end);
+    int s=0;
     for(int i = 0 ; i < n ; i++){
         cout <<v[i] <<" "; 
+        s+=v[i];
     }
+    cout << "\nSUM=" << s << "\n";
     return 0;
 }
